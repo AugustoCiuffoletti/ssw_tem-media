81d98cd89f44a019b81eaa10a33723a27fbb4b4f
AugustoCiuffoletti <augusto@di.unipi.it>
2021-03-22 09:30:32 +0100

    2 files modified

--- index.js
+++ index.js
@@ -5,61 +5,46 @@ var cityElems = document.getElementsByClassName("città");
 for (let elem of cityElems ) {
   elem.onclick = () => display(elem.innerHTML);
 }
+var t=0;
 media();
 
-// Funzione collegata ai bottoni
-// "window" necessario in StackBlitz, può essere
-// omesso altrimenti
-function display(city) {
+function doCity(city, callback) {
+  console.log(city)
   var request = new XMLHttpRequest(); // Costruzione dell'oggetto "request"
-
   // Funzione callback invocata quando la request termina
   request.onload = function() {
     // funzione definita arrow
     if (request.status === 200) {
       var dataObject = JSON.parse(request.response);
-      document.getElementById("risposta").innerHTML =
-        "A " + city + " ci sono " + dataObject.main.temp + " gradi";
+      callback(dataObject)
     } else {
       document.getElementById("risposta").innerText = "Errore";
     }
-  };
-
+  }
   // Applico il metodo "open"
   request.open(
     "GET",
-    "https://api.openweathermap.org/data/2.5/weather?APPID=d0fda39104b3c7c45fe031a5392964c1&units=metric&q=" +
-      city,
+    "https://api.openweathermap.org/data/2.5/weather?APPID=d0fda39104b3c7c45fe031a5392964c1&units=metric&q=" + city,
     true
   );
   // Applico il metodo send (al termine chiamerà il callback "onload")
   request.send();
-};
+}
+
+// Funzione collegata ai bottoni
+// "window" necessario in StackBlitz, può essere
+// omesso altrimenti
+function display(city) {
+  doCity( city, (data) => { 
+    document.getElementById("risposta").innerHTML =
+        "A " + city + " ci sono " + data.main.temp + " gradi";return 1 } ) ;
+}
 
 function media()  {
-  var c;
-  var t=0;
-  for ( let c of cityElems ) {
-    let request = new XMLHttpRequest(); // Costruzione dell'oggetto "request"
-
-  // Funzione callback invocata quando la request termina
-    request.onload = function() {
-    // funzione definita arrow
-      if (request.status === 200) {
-        var dataObject = JSON.parse(request.response);
-        console.log(dataObject.main.temp);
-        t += dataObject.main.temp/cityElems.length;
-        document.getElementById("media").innerText = t;
-      } else {
-        document.getElementById("risposta").innerText = "Errore";
-      }
-    };
-    request.open(
-      "GET",
-      "https://api.openweathermap.org/data/2.5/weather?APPID=d0fda39104b3c7c45fe031a5392964c1&units=metric&q=" +
-      c.innerHTML,
-      true
-    );
-    request.send()
+  for ( let city of cityElems ) {
+    doCity( city, (data) => {
+      t += data.main.temp/cityElems.length;
+      document.getElementById("media").innerText = t;
+    })
   }
 }
